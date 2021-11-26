6a87c51b9aab4224ae44e936218ab95ad40347eb
kimanh168 <kimanhlagi14@gmail.com>
2021-11-26 19:07:31 +0700

    lan chin

--- thongtinsp.php
+++ thongtinsp.php
@@ -1,35 +1,6 @@
 <?php include "header.php" ?>
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		 <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-
-		<title>Electro - HTML Ecommerce Template</title>
-
- 		<!-- Google font -->
- 		<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
-
- 		<!-- Bootstrap -->
- 		<link type="text/css" rel="stylesheet" href="css/bootstrap.min.css"/>
-
- 		<!-- Slick -->
- 		<link type="text/css" rel="stylesheet" href="css/slick.css"/>
- 		<link type="text/css" rel="stylesheet" href="css/slick-theme.css"/>
-
- 		<!-- nouislider -->
- 		<link type="text/css" rel="stylesheet" href="css/nouislider.min.css"/>
-
- 		<!-- Font Awesome Icon -->
- 		<link rel="stylesheet" href="css/font-awesome.min.css">
-
- 		<!-- Custom stlylesheet -->
- 		<link type="text/css" rel="stylesheet" href="css/style.css"/>
-
-    </head>
-		<?php
+	<body>
+        <?php
         if(isset($_GET['id'])):
             $id = $_GET['id'];
             $laySanPham=$product->getProductById($id);
@@ -41,123 +12,53 @@
 			<div class="container">
 				<!-- row -->
 				<div class="row">
-					<!-- Product main img -->
-					<div class="col-md-5 col-md-push-2">
-						<div id="product-main-img">
-							<div class="product-preview">
-								<img src="./img/<?php echo $value['image'] ?>" alt="">
-							</div>
-							
-						</div>
-					</div>
-					<!-- /Product main img -->
 
-					<!-- Product thumb imgs -->
-					<div class="col-md-2  col-md-pull-5">
-						<div id="product-imgs">
-							<div class="product-preview">
-								<img src="./img/<?php echo $value['image'] ?>" alt="">
+					<div class="col-md-7">
+						<!--  Details -->
+						<div class="billing-details">
+							<div class="section-title">
+								<h3 class="title"><?php echo $value['name'] ?></h3>
+							</div>
+							<div class="product-img">
+								<img src="./img/<?php echo $value['image'] ?>" width="400px">
 							</div>
 						</div>
+						<!--  Details -->
 					</div>
-					<!-- /Product thumb imgs -->
 
-					<!-- Product details -->
-					<div class="col-md-5">
-						<div class="product-details">
-							<h2 class="product-name"><?php echo $value['name'] ?></h2>
-							<div>
-								<div class="product-rating">
-									<i class="fa fa-star"></i>
-									<i class="fa fa-star"></i>
-									<i class="fa fa-star"></i>
-									<i class="fa fa-star"></i>
-									<i class="fa fa-star-o"></i>
+					<!-- Order Details -->
+					<div class="col-md-5 order-details">
+						<div class="section-title text-center">
+							<h3 class="title">Thông Tin</h3>
+						</div>
+						<div class="order-summary">
+                        <div class="order-col">
+								<div><strong>Giá</strong></div>
+								<div><strong class="order-total"><?php  echo number_format( $value['price'])?>VND</strong></div>
+							</div>
+							<div class="order-products">
+                                <div class="order-col">
+								    <div><strong>Loại</strong></div>
+								    <div><strong><?php  echo $value['type_name']?></strong></div>
+							    </div>
+								<div class="order-col">
+									<div><strong>Hãng</strong></div>
+									<div><strong><?php  echo $value['manu_name']?></strong></div>
 								</div>
-								<a class="review-link" href="#">10 Review(s) | Add your review</a>
 							</div>
-							<div>
-								<h3 class="product-price"><?php  echo number_format( $value['price'])?>VND</h3>
-								
-							</div>
-							<p></p>
-
-							<div class="product-options">
-								<label>
-									Size
-									<select class="input-select">
-										<option value="0">X</option>
-									</select>
-								</label>
-								<label>
-									Color
-									<select class="input-select">
-										<option value="0">Red</option>
-									</select>
-								</label>
-							</div>
-
-							<div class="add-to-cart">
-								<div class="qty-label">
-									Qty
-									<div class="input-number">
-										<input type="number">
-										<span class="qty-up">+</span>
-										<span class="qty-down">-</span>
-									</div>
-								</div>
-								<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
-							</div>
-
-							<ul class="product-btns">
-								<li><a href="#"><i class="fa fa-heart-o"></i> add to wishlist</a></li>
-								<li><a href="#"><i class="fa fa-exchange"></i> add to compare</a></li>
-							</ul>
-
-							<ul class="product-links">
-								<li>Category:</li>
-								<li><?php echo $value['manu_name'] ?></li>
-								<li><?php echo $value['type_name'] ?></li>
-							</ul>
-
 						</div>
-					</div>
-					<!-- /Product details -->
-					
-					<!-- Product tab -->
-					<div class="col-md-12">
-						<div id="product-tab">
-							<!-- product tab nav -->
-							<ul class="tab-nav">
-								<li class="active"><a data-toggle="tab" href="#tab1">Description</a></li>
-
-								
-							</ul>
-							<!-- /product tab nav -->
-
-							<!-- product tab content -->
-							<div class="tab-content">
-								<!-- tab1  -->
-								<div id="tab1" class="tab-pane fade in active">
-									<div class="row">
-										<div class="col-md-12">
-											<p><?php echo $value['description'] ?></p>
-										</div>
-									</div>
-								</div>
-								<!-- /tab1  -->
-
-							</div>
-							<!-- /product tab content  -->
+						<div class="payment-method">
+                        <h4 class="title">Bài Viết Đánh Giá</h4></br>
+                        <p><?php echo $value['description'] ?></p>
 						</div>
+						<a href="#" class="primary-btn order-submit"><i class="fa fa-shopping-cart"></i> add to cart</a>
 					</div>
-					<!-- /product tab -->
+					<!-- /Order Details -->
 				</div>
 				<!-- /row -->
 			</div>
 			<!-- /container -->
 		</div>
 		<!-- /SECTION -->
-		<?php endforeach; endif ?>
-		
-		<?php include "footer.html" ?>
+        <?php endforeach; endif ?>
+<?php include "footer.html" ?>
