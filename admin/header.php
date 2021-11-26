2b4c648a1114a7ba8fcd1cc8575b5740125b7dee
kimanh168 <kimanhlagi14@gmail.com>
2021-11-26 17:32:26 +0700

    lan chin

--- admin/header.php
+++ admin/header.php
@@ -247,7 +247,7 @@ $manufacture = new Manufacture;
                 </a>
               </li>
               <li class="nav-item">
-                <a href="protypeadd.php" class="nav-link">
+                <a href="protypeadd.php" class="nav-link <?php if($page == 'protypeadd'){echo 'active';}?>">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Add Protype</p>
                 </a>
@@ -261,7 +261,7 @@ $manufacture = new Manufacture;
               <li class="nav-item">
                 <a href="manufactureadd.php" class="nav-link <?php if($page == 'manufactureadd'){echo 'active';}?>">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Manufacture Add</p>
+                  <p>Add Manufacture </p>
                 </a>
               </li>
         </ul>
--- admin/models/product.php
+++ admin/models/product.php
@@ -27,4 +27,5 @@ class Product extends Db{
         return  $sql->execute();
     } 
     
+    
 }
