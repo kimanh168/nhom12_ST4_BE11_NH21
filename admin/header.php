a0ab45d5324edff2e34a85d54330972bf8ecdf3f
HoangThiHienn <wind07.15.2002@gmail.com>
2021-12-08 09:15:13 +0700

    dangnhap

--- admin/header.php
+++ admin/header.php
@@ -162,17 +162,9 @@ $manufacture = new Manufacture;
           <div class="dropdown-divider"></div>
           <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
         </div>
+        <a href="logout.php">Logout</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-          <i class="fas fa-expand-arrows-alt"></i>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
-          <i class="fas fa-th-large"></i>
-        </a>
-      </li>
+      
     </ul>
   </nav>
   <!-- /.navbar -->
