6a568f136414403d7ebe64af5a684243809c01de
kimanh168 <kimanhlagi14@gmail.com>
2021-12-22 07:23:35 +0700

    lan cuoi cun1g

--- admin/delproduct.php
+++ admin/delproduct.php
@@ -2,7 +2,7 @@
 require "config.php";
 require "models/db.php";
 require "models/product.php";
-$product = new Wishlist;
+$product = new Product;
     if(isset($_GET['id'])){
         $product -> deleteProduct($_GET['id']); 
         header('location: products.php');      
