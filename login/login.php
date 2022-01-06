f04d4f0e9c494f63af0ff2d5d6c9f7daec4f67ee
kimanh168 <kimanhlagi14@gmail.com>
2022-01-06 18:47:08 +0700

    lan cuoi cc

--- login/login.php
+++ login/login.php
@@ -4,7 +4,7 @@ require "../config.php";
 require "../models/db.php";
 require "../models/user.php";
 $user=new User();
-if  (isset($_POST['submit'])) {
+if(isset($_POST['submit'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
     if ($user -> checkLogin($username, $password)){
